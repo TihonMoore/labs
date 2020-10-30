c99830c06deae73d3c172832c5bedc8d9c27aa65
Moore Tihon <varkabastet@gmail.com>
2020-10-30 13:37:23 +0300

    Add lab3, lab4, lab6 ^^

---
+++ cmake-build-debug/CMakeFiles/LabsJob.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Tihon/CLionProjects/LabsJob/lab1.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/lab1.c.obj"
+  "C:/Users/Tihon/CLionProjects/LabsJob/lab2.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/lab2.c.obj"
+  "C:/Users/Tihon/CLionProjects/LabsJob/lab3.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/lab3.c.obj"
+  "C:/Users/Tihon/CLionProjects/LabsJob/lab4.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/lab4.c.obj"
+  "C:/Users/Tihon/CLionProjects/LabsJob/lab5.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/lab5.c.obj"
+  "C:/Users/Tihon/CLionProjects/LabsJob/lab6.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/lab6.c.obj"
+  "C:/Users/Tihon/CLionProjects/LabsJob/main.c" "C:/Users/Tihon/CLionProjects/LabsJob/cmake-build-debug/CMakeFiles/LabsJob.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LabsJob.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LabsJob.dir/lab1.c.obj"
+  "CMakeFiles/LabsJob.dir/lab2.c.obj"
+  "CMakeFiles/LabsJob.dir/lab3.c.obj"
+  "CMakeFiles/LabsJob.dir/lab4.c.obj"
+  "CMakeFiles/LabsJob.dir/lab5.c.obj"
+  "CMakeFiles/LabsJob.dir/lab6.c.obj"
+  "CMakeFiles/LabsJob.dir/main.c.obj"
+  "LabsJob.exe"
+  "LabsJob.exe.manifest"
+  "LabsJob.pdb"
+  "libLabsJob.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LabsJob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
